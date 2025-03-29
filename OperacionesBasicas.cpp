006de0883ead2c72a13d5a49cb96dfe4611d1676
unknown <ricardoarrecisrivera@gmail.com>
2025-03-28 21:48:41 -0600

    Se crea archivo de operaciones basicas

---
+++ OperacionesBasicas.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdlib>  // Para usar system("cls")
+using namespace std;
+
+int main() {
+    double num1, num2;
+    int opcion;
+
+    do {
+        cout << endl;
+        cout << "Seleccione una opcion:" << endl;
+        cout << "1. Sumar" << endl;
+        cout << "2. Restar" << endl;
+        cout << "3. Multiplicar" << endl;
+        cout << "4. Dividir" << endl;
+        cout << "5. Salir" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        if (opcion != 5) {
+            cout << "Introduce el primer numero: ";
+            cin >> num1;
+            cout << "Introduce el segundo numero: ";
+            cin >> num2;
+        }
+
+        switch (opcion) {
+            case 1: {
+                double suma = num1 + num2;
+                cout << "Suma: " << suma << endl;
+                break;
+            }
+            case 2: {
+                double resta = num1 - num2;
+                cout << "Resta: " << resta << endl;
+                break;
+            }
+            case 3: {
+                double multiplicacion = num1 * num2;
+                cout << "Multiplicacion: " << multiplicacion << endl;
+                break;
+            }
+            case 4: {
+                if (num2 != 0) {
+                    double division = num1 / num2;
+                    cout << "Division: " << division << endl;
+                } else {
+                    cout << "No se puede dividir por cero." << endl;
+                }
+                break;
+            }
+            case 5: {
+                cout << "Saliendo del programa..." << endl;
+                break;
+            }
+            default:
+                cout << "Opcion invalida. Por favor elige una opcion valida." << endl;
+        }
+
+        cout << "Presiona Enter para continuar...";
+        cin.ignore();  // Limpiar el buffer
+        cin.get();     // Esperar a que el usuario presione Enter
+
+        // Limpiar la consola
+        system("cls");
+
+    } while (opcion != 5); // Repetir hasta que elija salir
+
+    return 0;
+}
+
